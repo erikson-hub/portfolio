509244d9182e628e893dc254729d9ba227ad6cf3
Yasho <erikson.velro@gmail.com>
2024-02-14 02:32:17 -0500

    fix: projects images

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -7,13 +7,17 @@ const Projects = () => {
       {ProjectItems.map((ProjectItem) => (
         <article
           key={ProjectItem.id}
-          className='rounded-md bg-white dark:bg-[#2C353D]'>
-          <a href=''>
-            <img src={ProjectItem.img} alt='' className='' />
+          className='rounded-md bg-white dark:bg-[#2C353D] outline'>
+          <a href={ProjectItem.url} target='_blank'>
+            <img
+              src={ProjectItem.img}
+              alt={ProjectItem.name}
+              className='h-[390px] w-full object-cover'
+            />
           </a>
           <section className='px-2 py-2'>
             <h4 className='font-GraphikLight'>{ProjectItem.name}</h4>
-            <p>{ProjectItem.description}</p>
+            <p className='my-2'>{ProjectItem.description}</p>
             <div>
               <ul className='flex gap-2'>
                 {ProjectItem.techs.map((tech) => (
